d95fc091ed99ba39ed7eb1a466cc09fcc274e642
Diego <755232@unizar.es>
2019-04-02 15:10:43 +0200

    update

--- practicas/practica_2/practica2_6.sh
+++ practicas/practica_2/practica2_6.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 #Nombre autor: Diego Marco
 #NIP autor: 755232
+#-----------------------------------almaceno directorios con mismo patron-------------------------
+dirs=$(find $HOME -name bin???)
+if [ -z "$dirs" ];then 
+  #caso ningun directorio con mismo patron
+  ruta=$(mktemp -d $HOME/binXXX)
+  echo "Se ha creado el directorio $ruta"
+else
+  #caso uno o mas directorios con mismo patron
+  ruta=$(stat -c "%n,%Y" $dirs | sort -n -t , -k 2 | grep -o ".*," | tr -d "," | head -n 1)
+fi
+echo "Directorio destino de copia: "$ruta""
 
-#--------------------------creo fichero temporal----------------------------------------------
-file=$(mktemp -d $HOME/binXXX)
-echo "Se ha creado el directorio $file"
-#--------------------------Compruebo que es el directorio destino-----------------------------
-echo "Directorio destino de copia: $file"
-#--------------------------Cuento ficheros ejecutables----------------------------------------
-contador=0
+   contador=0
 for var in $(ls);do
-  if [ -x $var ] && [ -f $var ];then
-    contador=$(($contador + 1))
+  if [ -f $var ] && [ -x $var ];then
+    cp "$var" "$ruta"
+    echo "./$var ha sido copiado a $ruta"
+   contador=$(($contador + 1))
   fi
 done
-#--------------------------Copio ficheros ejecutables------------------------------------------
-if [ "$contador" -eq "1" ];then
-  #caso 0 ficheros ejecutables aparte de este en el directorio actual
-  echo "No se ha copiado ningun archivo"
+if [ $contador -eq 0 ];then
+  "No se ha copiado ningun archivo"
 else
-  #caso 1 o mas ficheros ejecutables en el directorio actual
-  for var in $(ls);do
-    if [ -x $var ] && [ -f $var ];then
-      cp "$var" "$file"
-      echo "./$var ha sido copiado a $file"
-    fi
-  done
   echo "Se han copiado $contador archivos"
 fi
-rm -r "$file"
+rm -r "$ruta"
+
