9e40b4b3f8f8e40c98eeb46c63fb1f78b7b2da8e
Diego <755232@unizar.es>
2019-04-03 12:01:44 +0200

    update prac3.txt apartado b

--- practicas/practica_3/practica_3.sh
+++ practicas/practica_3/practica_3.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if [ $(id -u) -eq 0 ];then
-  echo "BIEN!"
+#nombre autor:Diego Marco Beisty
+#nip autor: 755232
+cat /etc/shadow &> /dev/null
+#intento ejecutar cat sobre un fichero protegido como shadow
+#Compruebo el estado de salida del comando
+if [ $? -eq 0 ];then
+  #caso tiene privilegios de administración
   exit 0
 else 
-  echo "MAL"
+  #caso no tiene privilegios de administración
+  echo "Este script necesita privilegios de administracion"
   exit 1
 fi
