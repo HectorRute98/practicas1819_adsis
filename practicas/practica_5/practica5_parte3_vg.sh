16483c1b948bb992e1c9250cb2ad762794623241
Diego <755232@unizar.es>
2019-05-21 15:37:04 +0200

    prac5 medio bien

--- practicas/practica_5/practica5_parte3_vg.sh
+++ practicas/practica_5/practica5_parte3_vg.sh
@@ -2,15 +2,21 @@
 #Autor: Diego Marco 
 #Nip:755232
 
+if [ $# -lt 2 ];then
+	#caso numero de parametros incorrecto
+	echo "$0 <grupo volumen <particion> {particion}"
+	exit 1
+fi >&2
+#numero de parametros correcto
 grupo=$(sudo vgscan)
 existe=$(echo $grupo | grep "$1")
 if [-z "$existe"];then
-	echo "No existe el vg $1"
+	#caso no existe el grupo volumen
+	echo "No existe el grupo volumen $1"
 	exit 1
-else
-	for particion in "$@";do
-		sudo pvcreate "$particion"
-		sudo vgextend "$1 $particion"
-	done
-fi
-exit
+fi >&2
+for particion in "$@";do
+	sudo pvcreate "$particion"
+	sudo vgextend "$1 $particion"
+done
+exit 0
